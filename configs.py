ea33e491a6f1dbfebd929a48bf2aeed511a66d47
Thiti Dev <thiti.mwk.main@gmail.com>
2021-02-26 23:54:07 +0700

    :wrench: Created bot_configs

---
+++ configs.py
@@ -0,0 +1,20 @@
+bot_configs = {
+    "collecting_data": {
+        "sugar":{
+            "img_name": "sugar_area.png",
+            "confidence": 0.975
+        },
+        "jelly":{
+            "img_name": "jelly_area.png",
+            "confidence": 0.965            
+        },
+        "smith":{
+            "img_name": "smith_area.png",
+            "confidence": 0.96            
+        },
+        "lumberjack":{
+            "img_name": "lumberjack_area.png",
+            "confidence": 0.955
+        }
+    }
+}
